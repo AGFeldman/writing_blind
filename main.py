ea0ca11254fa4b8a59892359f92be38786d785b5
Aaron Feldman <feldmando@gmail.com>
2013-12-30 20:42:00 -0800

    Updated some naming conventions.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ cur_path = path.dirname(path.realpath('main.py')) + '/'
 # spaces in them.
 cur_path_with_slash = '\ '.join(cur_path.split(' '))
 with open(cur_path + 'backup_path.txt', 'rU') as file_:
-    backupPath = file_.readline().strip()
+    backup_path = file_.readline().strip()
 
 pphrase = getpass('Enter passphrase, or enter \'o\' or \'s\': ')
 
@@ -46,8 +46,8 @@ def crack(file_path, file_name, key=pphrase):
     decrypt_cmd = 'mcrypt -d -q ' + full_name_nc + ' -k ' + key
     call(decrypt_cmd, shell=True)
     # check to see if decryption was successful
-    filelist = listdir(remove_slashes(file_path)+'.')
-    decryptSuccess = remove_slashes(file_name) in filelist
+    file_list = listdir(remove_slashes(file_path)+'.')
+    decryptSuccess = remove_slashes(file_name) in file_list
     if not decryptSuccess:
         print 'Decryption failed.'
         exit(1) 
@@ -55,11 +55,11 @@ def crack(file_path, file_name, key=pphrase):
         rm_old_nc_cmd = 'shred -u ' + full_name_nc   
         call(rm_old_nc_cmd, shell=True)
 
-def open_helper(file_path, file_name, openWith):
+def open_helper(file_path, file_name, open_with):
     '''Opens the file denoted by FILE_NAME and FILE_PATH using the bash
     command denoted by OPENWITH.
     '''
-    open_cmd = openWith + ' ' + file_path + file_name
+    open_cmd = open_with + ' ' + file_path + file_name
     call(open_cmd, shell=True)
 
 def seal(file_path, file_name, key=pphrase):
@@ -77,10 +77,10 @@ def backup(file_path, file_name):
     '''
     full_name = file_path + file_name
     full_name_nc = full_name + '.nc'
-    backup = backupPath + file_name + '.nc'
+    backup = backup_path + file_name + '.nc'
     copy2(remove_slashes(full_name_nc), remove_slashes(backup))
 
-def open_(file_path, file_name, openWith, should_backup, key=pphrase):
+def open_(file_path, file_name, open_with, should_backup, key=pphrase):
     '''Decrypts the file denoted by FILE_NAME and FILE_PATH; opens it using
     the bash command OPENWITH; and then re-encrypts and backs up  the file
     when it is closed.
@@ -88,13 +88,13 @@ def open_(file_path, file_name, openWith, should_backup, key=pphrase):
     full_name = file_path + file_name
     if path.exists(remove_slashes(full_name + '.nc')):
         crack(file_path, file_name, key)
-        open_helper(file_path, file_name, openWith)
+        open_helper(file_path, file_name, open_with)
         seal(file_path, file_name, key)
         if should_backup:
             backup(file_path, file_name)
     elif path.exists(remove_slashes(full_name)):
         print 'Note:', file_name, 'not found encrypted. Using unencrypted.'
-        open_helper(file_path, file_name, openWith)
+        open_helper(file_path, file_name, open_with)
     else:
         print file_name, 'not found.'
 
@@ -103,42 +103,42 @@ def get_from_XML(XML_file):
     '''
     tree = ET.parse(cur_path + XML_file)
     root = tree.getroot()
-    filelist = []    
+    file_list = []    
     for child in root:
         appendThis = [child.attrib['name'], {}]
         for part in child:
             appendThis[1][part.tag] = part.text
-        filelist.append(appendThis)
-    return filelist
+        file_list.append(appendThis)
+    return file_list
 
 if path.exists(cur_path + 'info.xml.nc'):
     print 'hi'
     crack(cur_path_with_slash, 'info.xml')
-    filelist = get_from_XML('info.xml')
+    file_list = get_from_XML('info.xml')
     seal(cur_path_with_slash, 'info.xml')
 else:  # assume info.xml is not encrypted
-    filelist = get_from_XML('info.xml')
+    file_list = get_from_XML('info.xml')
 
 if action == 'normal' or action == 'o':
     if action == 'normal':
-        srcDict = filelist[0][1]
+        src_dict = file_list[0][1]
     elif action == 'o':
-        for i in range(len(filelist)):
-            output = str(i) + '     ' + filelist[i][0]
+        for i in range(len(file_list)):
+            output = str(i) + '     ' + file_list[i][0]
             print output
         num = int(raw_input('Choose a file number: '))
-        srcDict = filelist[num][1]
-    file_path = srcDict['path']
-    file_name = srcDict['name']
-    opener = srcDict['openwith']
-    should_backup = int(srcDict['backup'])
+        src_dict = file_list[num][1]
+    file_path = src_dict['path']
+    file_name = src_dict['name']
+    opener = src_dict['openwith']
+    should_backup = int(src_dict['backup'])
     open_(file_path, file_name, opener, should_backup)
 
 elif action == 's':
-    srcDict = filelist[0][1]
-    file_name = srcDict['name']
-    file_path = srcDict['path']
-    should_backup = int(srcDict['backup'])
+    src_dict = file_list[0][1]
+    file_name = src_dict['name']
+    file_path = src_dict['path']
+    should_backup = int(src_dict['backup'])
     full_name = file_path + file_name
     crack(file_path, file_name)
     thefile = open(full_name, 'a')
